7ee1cdcfe10915ed57694e9cc7712b095211ff82
Mark Mackay <Mark_Mackay@yahoo.com>
2018-12-12 20:23:40 +0000

    Finished mvp and most extensions

--- students_controller.rb
+++ students_controller.rb
@@ -5,15 +5,50 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
+get '/students' do # index
+  @students = Student.all()
+  erb( :index )
+end
+# new
+get '/students/new' do # new
+  erb( :new )
+end
+# post '/students/:id' do # update
+#   Student.new( params ).update
+#   redirect to '/students'
+# end
+
+post '/students' do # create
+  @student = Student.new( params )
+  @student.save()
+  erb( :create )
+end
 
 # show
+get '/students/:id' do # show
+  @student = Student.find( params[:id] )
+  erb( :show )
+end
 
-# new
+post '/students/:id' do # update
+  Student.new( params ).update
+  redirect to '/students'
+end
+
 
 # create
 
 # edit
+get '/students/:id/edit' do # edit
+  @student = Student.find( params[:id] )
+  erb( :edit )
+end
 
 # update
 
 # destroy
+post '/students/:id/delete' do # delete
+  student = Student.find( params[:id] )
+  student.delete()
+  redirect to '/students'
+end
